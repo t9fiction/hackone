ebfc637172a273db6362d98ce34ec907ad8ead96
t9fiction <sohail.sohailishaq@gmail.com>
2023-05-26 03:06:55 +0500

    promotions complete

--- src/components/Promotions.tsx
+++ src/components/Promotions.tsx
@@ -13,20 +13,68 @@ const Promotions = () => {
         </h2>
         <div className="py-4 flex flex-row space-x-6">
           <div className="w-[44%] space-y-2">
-            <div className="w-full h-44 bg-[#D6D6D8] flex flex-row items-center px-16">
+            <div className="w-full h-48 bg-[#D6D6D8] flex flex-row items-center px-16">
               <div className="w-[40%]">
                 <h3 className="text-primary font-bold text-2xl">GET UP TO</h3>
                 <h3 className="text-primary font-bold text-3xl">60%</h3>
                 <p className="text-primary text-lg">For the summer season</p>
               </div>
               <div className="w-[60%] overflow-hidden">
-                <Image src={'/images/promotions/event1.webp'} width={280} height={280} alt="promo-girl" />
+                <Image
+                  src={"/images/promotions/event1.webp"}
+                  width={300}
+                  height={300}
+                  alt="promo-girl"
+                />
               </div>
             </div>
-            <div className="w-full h-44 bg-primary"></div>
+            <div className="w-full h-48 bg-primary ">
+              <div className="items-center flex flex-col">
+                <h2 className="font-bold text-4xl text-white pb-4 pt-8">
+                  GET 30% Off
+                </h2>
+                <p className="text-white pb-2">USE PROMO CODE</p>
+                <button className="uppercase text-white py-2 px-8 rounded-md bg-[#474747] tracking-[3px]">
+                  dineweekendsale
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="w-[28%] bg-[#EFE1C7] h-88">row</div>
-          <div className="w-[28%] bg-[#D7D7D9] h-88">row</div>
+          <div className="w-[28%] bg-[#EFE1C7] h-[390px]">
+            <div className="pt-4">
+              <p className="px-4">Flex Sweatshirt</p>
+              <div className="flex px-4 flex-row space-x-2">
+                <p className="text-lg line-through">$100.00 </p>
+                <p className="text-lg font-bold">$75.00</p>
+              </div>
+              <div className="items-center justify-center flex overflow-hidden">
+                <Image
+                  src={"/images/promotions/event2.webp"}
+                  alt="promotion2"
+                  width={250}
+                  height={250}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="w-[28%] bg-[#D7D7D9] h-[390px]">
+          <div className="pt-4">
+              <p className="px-4">Flex Sweatshirt</p>
+              <div className="flex px-4 flex-row space-x-2">
+                <p className="text-lg line-through">$225.00 </p>
+                <p className="text-lg font-bold">$190.00</p>
+              </div>
+              <div className="items-center justify-center  flex">
+                <Image
+                  src={"/images/promotions/event3.webp"}
+                  alt="promotion2"
+                  width={250}
+                  height={250}
+                  className="overflow-hidden"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
