1f45f4144469f77294c3d9284662d8da78e2ce90
t9fiction <sohail.sohailishaq@gmail.com>
2023-05-31 12:43:01 +0500

    updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ import { urlFor } from "@/functions/allfunction";
 
 const Card = ({ product }: any) => {
   return (
-    <div className="flex flex-col shrink-0 px-4" id={product.id}>
+    <div className="shrink-0 px-4" id={product.id}>
       <img src={urlFor(product.image).url()} alt="" className="hover:scale-105 ease-in-out duration-300 rounded-md h-[320px]" />
       <h3 className="font-light md:font-bold text-lg pt-0 sm:pt-1 md:pt-2 lg:pt-4">{product.name}</h3>
       <h2 className="md:font-semibold text-base  md:text-lg lg:text-xl">$ {product.price}</h2>
