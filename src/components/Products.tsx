7ecdef2b53f581e25a8ffce3da3a7ca4cc71d785
t9fiction <sohail.sohailishaq@gmail.com>
2023-06-05 02:59:01 +0500

    slider main configured

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -8,31 +8,27 @@ import { client } from "../../sanity/lib/client";
 import { Product } from "../../types/Product";
 
 export const getData = async () => {
-  const response = await client.fetch(
-    `*[_type=="product" && special==true]`
-  );
+  const response = await client.fetch(`*[_type=="product" && special==true]`);
   return response;
 };
-
 
 const Products = async () => {
   const data = await getData();
   const responsive = {
-    
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 3,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
-  
+
   return (
     <div className="bg-white" id="products">
       <div className="container mx-auto px-6 sm:px-8 md:px-12 lg:px-16 py-8">
@@ -44,8 +40,13 @@ const Products = async () => {
         </h2>
 
         {/* Corousel */}
-        <Carousel responsive={responsive}>
-          {data.map((product:Product) => (
+        <Carousel
+          responsive={responsive}
+          showDots={true}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+        >
+          {data.map((product: Product) => (
             <div key={product._id}>
               <Card product={product} />
             </div>
